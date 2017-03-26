2047465d7cf6e3cd6be0ae079f60ca654762afb5
calidion <calidion@gmail.com>
2017-03-27 05:45:35 +0800

    enabled markdown editor

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,7 @@
-$(document).ready(function() {
-  $('a.post-item').click(function() {
+$(document).ready(function () {
+  var simplemde = new SimpleMDE({ element: $("[data-provide=markdown]")[0] });
+
+  $('a.post-item').click(function () {
     var data = $(this).attr('data');
     console.log(data);
     $('input[name=parent]').val(data);
