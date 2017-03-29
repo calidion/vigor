41a9a0cb780af3385a73e4912fba5368366eb26e
calidion <calidion@gmail.com>
2017-03-30 01:59:10 +0800

    enabled letenc

--- test/v2/site.js
+++ test/v2/site.js
@@ -42,13 +42,43 @@ describe('v2 site', function () {
       done(err);
     });
   });
+});
 
-  it('should be able to handler letenc autho', function (done) {
-    process.env.FORIM_LETSENC_VALUE = 'sdofosdfodf';
+describe('letenc', function () {
+  var key = 'key';
+  var value = 'value';
+
+  before(function (cb) {
+    var LetsEnc = app.models.LetsEnc;
+    LetsEnc.create({
+      key: key,
+      value: value
+    }).then(function (created) {
+      created.key.should.equal(key);
+      created.value.should.equal(value);
+      cb();
+    });
+  });
+
+  it('should be able to handler letenc auth', function (done) {
     var req = http(app).get('/.well-known/acme-challenge/sdfosofdosdoffd');
     req.end(function (err, res) {
+      console.log(err, res.text);
       res.status.should.equal(200);
-      res.text.should.containEql(process.env.FORIM_LETSENC_VALUE);
+      res.body.should.containDeepOrdered({
+        code: 5,
+        name: 'NotFound',
+        message: '未找到！'
+      });
+      done(err);
+    });
+  });
+
+  it('should be able to handler letenc auth', function (done) {
+    var req = http(app).get('/.well-known/acme-challenge/' + key);
+    req.end(function (err, res) {
+      res.status.should.equal(200);
+      res.text.should.containEql(value);
       done(err);
     });
   });
